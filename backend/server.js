c6abb1fd5c4d2b8b0d0cbd0e40340e1ebb384325
Alfonso Castro Echeverría <acastro73@hotmail.com>
2025-11-01 13:16:10 -0600

    Agregando CORS corregido

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,6 @@ const connectDB = require('./config/db');                   // Importación de l
 const errorHandler = require('./middleware/errorMiddleware'); // Importación del middleware de manejo de errores
 const cors = require('cors');                             // Importación del módulo cors para habilitar CORS
 
-// Configuración de CORS para permitir solicitudes desde cualquier origen
-app.use(cors());                                            // Habilita CORS para todas las rutas y orígenes
 
 // Configuración de la variable de entorno PORT, si no se encuentra, se usa el puerto 5000
 const port = process.env.PORT || 5000;                      // Tomar el valor de la variable de entorno PORT o 5000 si no se encuentra
@@ -19,6 +17,9 @@ connectDB();                                                // Llamada a la func
 
 // Crear una instancia de la aplicación Express, esta se utiliza para definir las rutas y middlewares del servidor.
 const app = express();                                      // Crear una instancia de express
+
+// Configuración de CORS para permitir solicitudes desde cualquier origen
+app.use(cors());                                            // Habilita CORS para todas las rutas y orígenes
 
 // Configuración de middlewares de Express
 app.use(express.json());                                    // Middleware para parsear datos de JSON
